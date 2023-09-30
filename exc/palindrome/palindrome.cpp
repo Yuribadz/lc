9f5ddc00c35635ebc27f696b80a9baeea1187440
Yuribadz <yuriy.badziuk@posteo.de>
2023-10-01 01:19:55 +0300

    Correct formatting

--- exc/palindrome/palindrome.cpp
+++ exc/palindrome/palindrome.cpp
@@ -2,22 +2,23 @@
 #include <algorithm>
 #include <iostream>
 
-
-class Solution {
- public:
-
-  bool isPalindrome(int x) { 
-	  std::string forward = std::to_string(x);
-    std::string reverse{forward};
-          std::reverse(reverse.begin(), reverse.end());
-    if (forward == reverse) return true;
-          return false;
-  }
+class Solution
+{
+public:
+      bool isPalindrome(int x)
+      {
+            std::string forward = std::to_string(x);
+            std::string reverse{forward};
+            std::reverse(reverse.begin(), reverse.end());
+            if (forward == reverse)
+                  return true;
+            return false;
+      }
 };
 
 int main()
 {
-    int num = 6556;
-    std::cout << Solution().isPalindrome(num) << std::endl;
-    return 0;
+      int num = 6556;
+      std::cout << Solution().isPalindrome(num) << std::endl;
+      return 0;
 }
