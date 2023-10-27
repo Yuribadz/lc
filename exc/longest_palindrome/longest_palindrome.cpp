9c96e84bfea02a2efe0dec33c779de1653103a02
Yuribadz <yuriy.badziuk@posteo.de>
2023-10-27 22:04:04 +0300

    Add proper my solution

--- exc/longest_palindrome/longest_palindrome.cpp
+++ exc/longest_palindrome/longest_palindrome.cpp
@@ -1,9 +1,39 @@
 #include <string>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <bitset>
 
-class Solution
+class Solution {
+public:
+    int longestPalindrome(std::string s) {
+        std::unordered_map<char,int> char_map;
+        for(const auto& elem: s) 
+        {
+            char_map[elem]++;
+        }
+
+        int count = 0;
+        bool odd = false;
+
+        for(const auto elem: char_map)
+        {
+            if(elem.second % 2 == 0) 
+            {
+                  count += elem.second;
+            }
+            else
+            {
+                count += elem.second-1;
+                odd = true;
+            }
+        }
+
+        return (odd) ? count + 1 : count;
+    }
+};
+
+class BitSolution
 {
 public:
       int longestPalindrome(std::string s)
@@ -17,9 +47,11 @@ public:
       }
 };
 
+
 int main()
 {
 
-      std::cout << Solution().longestPalindrome("a") << std::endl;
+      std::cout << Solution().longestPalindrome("abccccddfgh") << std::endl;
+      std::cout << BitSolution().longestPalindrome("abccccddfgh") << std::endl;
       return 0;
 }
