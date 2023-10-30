7dda60f2d76410c7b4274dfee336b9987dc4b6ea
Yuribadz <yuriy.badziuk@posteo.de>
2023-10-30 22:55:36 +0200

    fix bs_sorted

--- exc/bs_sorted/bs_sorted.cpp
+++ exc/bs_sorted/bs_sorted.cpp
@@ -9,17 +9,17 @@ public:
     int search(std::vector<int> &nums, int target)
     {
         int left = 0;
-        int right = nums.size();
+        int right = nums.size() - 1;
         int mid = 0;
 
-        while (left < right)
+        while (left <= right)
         {
             mid = left + (right - left) / 2;
             if (target == nums[mid])
                 return mid;
             if (target < nums[mid])
             {
-                right = mid;
+                right = --mid;
             }
             else
             {
@@ -34,6 +34,6 @@ public:
 int main()
 {
     std::vector<int> nums = {5};
-    std::cout << Solution().search(nums, 5) << std::endl;
+    std::cout << Solution().search(nums, 9) << std::endl;
     return 0;
 }
