9998da044fd1bc3f70b2f138da460aa2d99c9505
Yuribadz <yuriy.badziuk@posteo.de>
2023-10-30 22:40:09 +0200

    Add queue using stacks

---
+++ exc/queue_stacks/queue_stacks.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+class MyQueue
+{
+public:
+    void push(int value)
+    {
+        m_stack_push.push(value);
+    }
+
+    void move_to_pop()
+    {
+        if (m_stack_pop.empty())
+        {
+            while (!m_stack_push.empty())
+            {
+                m_stack_pop.push(m_stack_push.top());
+                m_stack_push.pop();
+            }
+        }
+    }
+    int pop()
+    {
+        int ret = peek();
+        m_stack_pop.pop();
+        return ret;
+    }
+
+    int peek()
+    {
+        if(m_stack_pop.empty() && m_stack_push.empty())
+        {
+            return INT_MIN;
+        }
+        move_to_pop();
+        return m_stack_pop.top();
+    }
+
+    bool empty()
+    {
+        move_to_pop();
+        return m_stack_pop.empty();
+    }
+
+private:
+    std::stack<int> m_stack_pop;
+    std::stack<int> m_stack_push;
+};
+
+int main()
+{
+    MyQueue que;
+    que.push(10);
+    que.push(20);
+    std::cout << que.pop() << std::endl;
+    std::cout << que.pop() << std::endl;
+    return 0;
+}
